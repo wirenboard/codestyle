f142dcafe7223d6a24acc3c83ab934df1b84618c
pete <petr.krasnoshchekov@wirenboard.ru>
2021-10-01 13:13:09 +0500

    clang-format for C++ and script for recursive format checking

---
+++ clang-format-subdirs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Apply dry run of clang-format to all *.h and *.cpp files in specified directories recursively."
+    echo "Usage: $0 DIR1, DIR2, ..."
+    exit 1
+fi
+
+FAIL=0
+
+for dir in "$@"; do
+    find ${dir} -iname *.h -o -iname *.cpp | xargs clang-format -n --Werror -i || FAIL=1
+done
+
+exit $FAIL
