edb4f30feec0d124e1eb5852d58700fb9f7f642e
Nikolay Korotkiy <sikmir@disroot.org>
2024-01-30 19:02:02 +0400

    Fix shellcheck

--- clang-format-subdirs.sh
+++ clang-format-subdirs.sh
@@ -11,7 +11,7 @@ FAIL=0
 clang-format --version
 
 for dir in "$@"; do
-    find ${dir} -iname '*.h' -o -iname '*.cpp' | xargs clang-format -n --Werror -i || FAIL=1
+    find "${dir}" -iname '*.h' -o -iname '*.cpp' -type f -print0 | xargs -0 clang-format -n --Werror -i || FAIL=1
 done
 
 exit $FAIL
