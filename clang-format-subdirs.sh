d70e4903488cde5adfc1e2e4866765717cec0e77
Nikolay Korotkiy <sikmir@disroot.org>
2024-01-31 11:46:32 +0400

    Add clang-tidy-subdirs.sh (#62)

--- clang-format-subdirs.sh
+++ clang-format-subdirs.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [ $# -eq 0 ]; then
     echo "Apply dry run of clang-format to all *.h and *.cpp files in specified directories recursively."
@@ -8,8 +8,10 @@ fi
 
 FAIL=0
 
+clang-format --version
+
 for dir in "$@"; do
-    find ${dir} -iname '*.h' -o -iname '*.cpp' | xargs clang-format -n --Werror -i || FAIL=1
+    find "${dir}" -iname '*.h' -o -iname '*.cpp' -type f -print0 | xargs -0 clang-format -n --Werror -i || FAIL=1
 done
 
 exit $FAIL
---
+++ clang-tidy-subdirs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]; then
+    echo "Apply dry run of clang-tidy to all *.h and *.cpp files in specified directories recursively."
+    echo "Usage: $0 DIR1, DIR2, ..."
+    exit 1
+fi
+
+FAIL=0
+
+clang-tidy --version
+
+for dir in "$@"; do
+    find "${dir}" -iname '*.h' -o -iname '*.cpp' -type f -print0 | xargs -0 clang-tidy --extra-arg="${CLANG_TIDY_EXTRA_ARG}" -p . || FAIL=1
+done
+
+exit $FAIL
