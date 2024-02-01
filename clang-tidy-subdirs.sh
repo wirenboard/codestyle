980ac989ebaa5cd28fea18c29e1401c92a60935b
Nikolay Korotkiy <sikmir@disroot.org>
2024-02-01 15:51:36 +0400

    Fix clang-*-subdirs.sh (#63)

--- clang-tidy-subdirs.sh
+++ clang-tidy-subdirs.sh
@@ -11,7 +11,7 @@ FAIL=0
 clang-tidy --version
 
 for dir in "$@"; do
-    find "${dir}" -iname '*.h' -o -iname '*.cpp' -type f -print0 | xargs -0 clang-tidy --extra-arg="${CLANG_TIDY_EXTRA_ARG}" -p . || FAIL=1
+    find "${dir}" \( -iname '*.h' -o -iname '*.cpp' \) -type f -print0 | xargs -0 clang-tidy --extra-arg="${CLANG_TIDY_EXTRA_ARG}" -p . || FAIL=1
 done
 
 exit $FAIL
