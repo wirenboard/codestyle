46fe92ebdd4cc81aa0c7d78fbe3ce4593fa8f5e2
Nikolay Korotkiy <sikmir@disroot.org>
2026-02-12 16:07:11 +0400

    vscode-go: add golangci-lint (#118)

--- go/local/linux-devenv.sh
+++ go/local/linux-devenv.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+cp ../codestyle/go/config/.golangci.yml ./
 [ -d ./.vscode ] || cp -r ../codestyle/go/vscode/.vscode/ ./
 
-GOVERSION=1.25.6
+GOVERSION=1.26.0
 
 curl -LO https://go.dev/dl/go$GOVERSION.linux-amd64.tar.gz
 tar -C /usr/local -xzf go$GOVERSION.linux-amd64.tar.gz
 rm -f go$GOVERSION.linux-amd64.tar.gz
 
 /usr/local/go/bin/go install github.com/go-delve/delve/cmd/dlv@latest
+/usr/local/go/bin/go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@latest
 /usr/local/go/bin/go install golang.org/x/tools/gopls@latest
 /usr/local/go/bin/go install gotest.tools/gotestsum@latest
